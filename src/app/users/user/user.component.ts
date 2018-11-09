cece82fa6b929c13ea1a6185004a1c0e4915ce1e
Will Reichl <William.Reichl@dcsg.com>
2018-11-09 06:58:30 -0500

    S11L126 - Route Observables Cleanup

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { } // access to currently loaded route
 
@@ -18,13 +20,21 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     console.log(this.user);
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params.id;
         this.user.name = params.name;
         console.log('Params changed!');
       }
     );
+  }
+
+  ngOnDestroy() {
+    // You do not need to do this, Angular takes care of this implicitly.
+    // This is just shown as an example of what you'd need to do explicitly.
+    this.paramsSubscription.unsubscribe();
+
+    // However, if you create your own observables, you're responsible for their destruction.
   }
 
 }
