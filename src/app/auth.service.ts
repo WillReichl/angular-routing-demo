56e2a062c6c530a12d41eecfe2944d1ceb9d5509
Will Reichl <William.Reichl@dcsg.com>
2018-11-12 13:35:36 -0500

    S11L137 - Protecting Routes with canActivate (using mock auth)

---
+++ src/app/auth.service.ts
@@ -0,0 +1,23 @@
+export class AuthService {
+    loggedIn = false;
+
+    // set up a "fake" to test authentication, make it take 8/10th of a second
+    isAuthenticated() {
+        const promise = new Promise(
+            (resolve, reject) => {
+                setTimeout(() => {
+                    resolve(this.loggedIn);
+                }, 800);
+            }
+        );
+        return promise;
+    }
+
+    login() {
+        this.loggedIn = true;
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
